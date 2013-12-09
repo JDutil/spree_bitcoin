68cf36b1497bd45da06b0c4326b5d1d332d7e1b6
Christian Genco <christian.genco@gmail.com>
2013-12-09 01:13:39 -0600

    more concise and working CoinbaseController#invoice

--- app/controllers/spree/coinbase_controller.rb
+++ app/controllers/spree/coinbase_controller.rb
@@ -2,44 +2,30 @@ module Spree
   class CoinbaseController < StoreController
 
     def invoice
-      # begin
-        Rails.logger.debug current_order.inspect
-        Rails.logger.debug current_order.total
-        Rails.logger.debug({
-            :button => {
-              :callback_url => spree.coinbase_notify_url,
-              :cancel_url => spree.checkout_state_url(:payment),
-              :custom => current_order.number,
-              :description => "#{current_order.line_items.map{|li| "#{li.quantity} x #{li.product.name} #{Spree::Money.new(li.price)}" }.join('\n')}",
-              :name => "#{Spree::Config[:site_name]} Order #{current_order.number}",
-              :type => 'buy_now',
-              :price => current_order.total.to_s,
-              :price_string => current_order.total.to_s,
-              :price_currency_ico => current_currency,
-              :include_email => true,
-              :success_url => spree.order_url(current_order)
-            }
-          }.to_json)
+      begin
+        order_total = BigDecimal.new(current_order.total)
+        order_total_string = order_total.to_s('F')
+
+        button_data = {
+          :name => "#{Spree::Config[:site_name]} Order #{current_order.number}",
+          :price => order_total_string,
+          :price_string => order_total_string,
+          :price_currency_iso => current_currency,
+          :callback_url => spree.coinbase_notify_url,
+          :cancel_url => spree.checkout_state_url(:payment),
+          :custom => current_order.number,
+          :description => "#{current_order.line_items.map{|li| "#{li.quantity} x #{li.product.name} #{Spree::Money.new(li.price)}" }.join('\n')}",
+          :include_email => true,
+          :type => 'buy_now',
+          :success_url => spree.order_url(current_order)
+        }
+
         response = HTTParty.post("https://coinbase.com/api/v1/buttons?api_key=#{payment_method.preferred_api_key}",
-          :body => {
-            :button => {
-              :callback_url => spree.coinbase_notify_url,
-              :cancel_url => spree.checkout_state_url(:payment),
-              :custom => current_order.number,
-              :description => "#{current_order.line_items.map{|li| "#{li.quantity} x #{li.product.name} #{Spree::Money.new(li.price)}" }.join('\n')}",
-              :include_email => true,
-              :name => "#{Spree::Config[:site_name]} Order #{current_order.number}",
-              :type => 'buy_now',
-              :price => current_order.total.to_s,
-              :price_currency_iso => current_currency,
-              :price_string => current_order.total.to_s,
-              :success_url => spree.order_url(current_order)
-            }
-          }.to_json,
+          :body => {:button => button_data}.to_json,
           :headers => { 'Content-Type' => 'application/json' }
         )
         data = response.parsed_response
-        Rails.logger.debug data
+
         if data['success'] == true
           code = data['button']['code']
           redirect_to "https://coinbase.com/checkouts/#{code}"
@@ -47,10 +33,10 @@ module Spree
           flash[:error] = "Coinbase Error: #{data['errors'].join(',')}"
           redirect_to checkout_state_path(:payment)
         end
-      # rescue
-      #   flash[:error] = "Could not connect to Coinbase."
-      #   redirect_to checkout_state_path(:payment)
-      # end
+      rescue
+        flash[:error] = "Could not connect to Coinbase."
+        redirect_to checkout_state_path(:payment)
+      end
     end
 
     def notify
